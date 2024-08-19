029f5721c6d88b3e0b2e6bc40314c65dc80aef81
amaanshahk <amaanshahk123@gmail.com>
2024-08-19 22:07:37 +0530

    Add pre-commit hook to run tests

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,25 @@
-const hello = () => {
-  console.log("Hello GitHub!");
-};
+// const fs = require("fs");
 
-hello();
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World. Welcome to Node.js File System module.",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created!");
+//   }
+// );
+// fs.readFile("sample.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+const args = require("minimist")(process.argv.slice(2), {
+  alias: {
+    n: "name",
+    a: "age",
+  },
+  default: {
+    greeting: "Hello",
+  },
+});
+
+console.log(args);
