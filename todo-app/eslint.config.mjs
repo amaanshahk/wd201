1b0f0f7f9f75d992fb5c36ba4e8cd5ce98873636
amaanshahk <amaanshahk123@gmail.com>
2024-08-24 08:33:27 +0530

    postgre and sequelize in l6

--- todo-app/eslint.config.mjs
+++ todo-app/eslint.config.mjs
@@ -1,13 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import { defineConfig } from "eslint";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
-    env: {
-      jest: true,
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
     },
   },
   pluginJs.configs.recommended,
