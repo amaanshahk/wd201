21b5d9a1f7ca5d64317db1a73cd3c7e97d745f65
amaanshahk <amaanshahk123@gmail.com>
2024-08-24 08:23:51 +0530

    postgre and sequelize in l6

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,1 +1,8 @@
-console.log("Printing this");
+const express = require("express");
+const app = express();
+
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
+
+app.listen(3000);
