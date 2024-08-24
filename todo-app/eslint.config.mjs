da1533806f355304803ca1c1b0e21f4bf349d5db
amaanshahk <amaanshahk123@gmail.com>
2024-08-24 07:37:23 +0530

    trying if eslint works

--- todo-app/eslint.config.mjs
+++ todo-app/eslint.config.mjs
@@ -5,18 +5,4 @@ export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  {
-    env: {
-      commonjs: true,
-      es2021: true,
-      node: true,
-      jest: true,
-    },
-    extends: "eslint:recommended",
-    overrides: [],
-    parserOptions: {
-      ecmaVersion: "latest",
-    },
-    rules: {},
-  },
 ];
