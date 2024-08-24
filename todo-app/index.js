3ca8402dc0c13fbafc0708ba35f2e814ee6b2fd5
amaanshahk <amaanshahk123@gmail.com>
2024-08-24 10:01:18 +0530

    routing is express l6

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,21 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
+app.get("/todos", (request, response) => {
+  //   response.send("Hello World");
+  console.log("To do list.");
 });
 
+app.post("/todos", (request, response) => {
+  console.log("Creating a to do.");
+});
+
+app.put("/todo/:id/markAsCompleted", (request, response) => {
+  console.log("Marking a to do as done by ID: ", request.param.id);
+});
+
+app.delete("/todo/:id", (request, response) => {
+  console.log("Delete to do by ID: ", request.params.id);
+});
 app.listen(3000);
