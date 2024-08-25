eb354f2daa68ad8b7847033187a8d7eb703b4179
amaanshahk <amaanshahk123@gmail.com>
2024-08-25 08:00:50 +0530

    creating a new to-do, and mark an existing to-do as completed

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,18 +1,40 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/todos", (request, response) => {
   //   response.send("Hello World");
   console.log("To do list.");
 });
 
-app.post("/todos", (request, response) => {
+app.post("/todos", async (request, response) => {
   console.log("Creating a to do.");
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
-app.put("/todo/:id/markAsCompleted", (request, response) => {
-  console.log("Marking a to do as done by ID: ", request.param.id);
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("Marking a to do as done by ID: ", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.delete("/todo/:id", (request, response) => {
