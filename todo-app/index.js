0837c6ec84ef37c0515bee16d3d3f38f5b50d570
amaanshahk <amaanshahk123@gmail.com>
2024-08-26 16:40:06 +0530

    Adding tests to Express.js application

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,43 +1,4 @@
-/* eslint-disable no-unused-vars */
-const express = require("express");
-const app = express();
-const { Todo } = require("./models");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-app.get("/todos", (request, response) => {
-  //   response.send("Hello World");
-  console.log("To do list.");
+const app = require("./app");
+app.listen(3000, () => {
+  console.log("Started express server.");
 });
-
-app.post("/todos", async (request, response) => {
-  console.log("Creating a to do.");
-  try {
-    const todo = await Todo.addTodo({
-      title: request.body.title,
-      dueDate: request.body.dueDate,
-      completed: false,
-    });
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
-
-app.put("/todos/:id/markAsCompleted", async (request, response) => {
-  console.log("Marking a to do as done by ID: ", request.params.id);
-  const todo = await Todo.findByPk(request.params.id);
-  try {
-    const updatedTodo = await todo.markAsCompleted();
-    return response.json(updatedTodo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
-
-app.delete("/todo/:id", (request, response) => {
-  console.log("Delete to do by ID: ", request.params.id);
-});
-app.listen(3000);
