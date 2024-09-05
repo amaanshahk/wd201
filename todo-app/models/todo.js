ebbd56693a750489442f6fe53f947573a28dc13d
amaanshahk <amaanshahk123@gmail.com>
2024-09-05 06:27:38 +0530

    L7 midway till rendering allTodos

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
     markAsCompleted() {
       return this.update({ completed: true });
     }
+
+    static getTodos() {
+      return this.findAll();
+    }
   }
   Todo.init(
     {
